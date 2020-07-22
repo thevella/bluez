68df9ac5337ace7848e085a5853d76e8653cfaf0
ruundii <alexey.outkine@gmail.com>
2020-07-22 09:39:18 +0100

    added connection/disconnection handling

--- profiles/input/host.c
+++ profiles/input/host.c
@@ -9,6 +9,9 @@
 #include "src/adapter.h"
 #include "src/device.h"
 #include "src/log.h"
+#include "btio/btio.h"
+#include "gdbus/gdbus.h"
+#include "src/dbus-common.h"
 
 
 struct input_host{
@@ -37,10 +40,41 @@ struct input_host{
 
 static GSList* hosts = NULL;
 
+static void shutdown_channels(struct input_host *host);
+
 static int cmp_host_addr(gconstpointer a, gconstpointer dst)
 {
     const struct input_host *host = a;
     return bacmp(&host->dst, dst);
+}
+
+static gboolean device_property_state_changed(DBusConnection *conn, DBusMessage *msg,
+                                               void *user_data) {
+     struct input_host *host = user_data;
+    bool connected = btd_device_is_connected(host->device);
+    if(!connected) shutdown_channels(host);
+    return TRUE;
+}
+
+
+static struct input_host *create_input_host(const bdaddr_t *src, const bdaddr_t *dst){
+    //create a new host
+    struct btd_device *device = btd_adapter_find_device(adapter_find(src), dst, BDADDR_BREDR);
+    const char *path = device_get_path(device);
+    struct input_host *new_host = g_new0(struct input_host, 1);
+    new_host->path = g_strdup(path);
+    bacpy(&new_host->src, src);
+    bacpy(&new_host->dst, dst);
+    new_host->device = btd_device_ref(device);
+    hosts= g_slist_append(hosts, new_host);
+
+
+    g_dbus_add_properties_watch(btd_get_dbus_connection(), NULL,
+                            new_host->path, DEVICE_INTERFACE,
+                            device_property_state_changed, new_host,
+                            NULL);
+
+    return new_host;
 }
 
 static struct input_host *find_host(const bdaddr_t *src, const bdaddr_t *dst, bool create_if_not_found){
@@ -52,54 +86,45 @@ static struct input_host *find_host(const bdaddr_t *src, const bdaddr_t *dst, bo
 
     if (host_list_item == NULL) {
         if(!create_if_not_found) return NULL;
-        //create a new host
-        const char *path = device_get_path(device);
-        struct input_host *new_host = g_new0(struct input_host, 1);
-        new_host->path = g_strdup(path);
-        bacpy(&new_host->src, src);
-        bacpy(&new_host->dst, dst);
-        new_host->device = btd_device_ref(device);
-        hosts= g_slist_append(hosts, new_host);
-        return new_host;
+        return create_input_host(src, dst);
     }
 
     return host_list_item->data;
 }
 
 static gboolean ctrl_watch_cb(GIOChannel *chan, GIOCondition cond, gpointer data) {
-    if (cond & G_IO_IN) {
+    struct input_host *host = data;
+
+    if (cond == G_IO_IN || cond == G_IO_PRI || cond == (G_IO_IN | G_IO_PRI)) {
         DBG("ctrl data is in ");
+        return TRUE;
     }
-    else if (cond & G_IO_PRI) {
-        DBG("ctrl prio data is in ");
+    else if(cond & G_IO_NVAL){
+        DBG("ctrl invalid message ");
+        return TRUE;
     }
-    else if (cond & G_IO_HUP) {
-        DBG("ctrl hang up ");
-    }
-    else if (cond & G_IO_ERR) {
-        DBG("ctrl error ");
-    }
-    else if (cond & G_IO_NVAL) {
-        DBG("ctrl data is invalid ");
-    }
+    char address[18];
+    ba2str(&host->dst, address);
+    DBG("Input host ctrl %s disconnected. Removing input host", address);
+    input_host_remove(&host->src, &host->dst);
+    return FALSE;
 }
 
 static gboolean intr_watch_cb(GIOChannel *chan, GIOCondition cond, gpointer data) {
-    if (cond & G_IO_IN) {
+    struct input_host *host = data;
+    if (cond == G_IO_IN || cond == G_IO_PRI || cond == (G_IO_IN | G_IO_PRI)) {
         DBG("intr data is in ");
+        return TRUE;
     }
-    else if (cond & G_IO_PRI) {
-        DBG("intr prio data is in ");
+    else if(cond & G_IO_NVAL){
+        DBG("intr invalid message ");
+        return TRUE;
     }
-    else if (cond & G_IO_HUP) {
-        DBG("intr hang up ");
-    }
-    else if (cond & G_IO_ERR) {
-        DBG("intr error ");
-    }
-    else if (cond & G_IO_NVAL) {
-        DBG("intr data is invalid ");
-    }
+    char address[18];
+    ba2str(&host->dst, address);
+    DBG("Input host intr %s disconnected. Removing input host", address);
+    input_host_remove(&host->src, &host->dst);
+    return FALSE;
 }
 
 
@@ -124,9 +149,43 @@ int input_host_set_channel(const bdaddr_t *src, const bdaddr_t *dst, int psm, GI
             host->intr_io = g_io_channel_ref(io);
             host->intr_watch = g_io_add_watch(host->intr_io, cond,
                                               intr_watch_cb, host);
+
+            /*GError *gerr = NULL;
+            if (!bt_io_set(host->intr_io, &gerr,
+                           BT_IO_OPT_SEC_LEVEL, BT_IO_SEC_MEDIUM,
+                           BT_IO_OPT_INVALID)) {
+                error("cannot set encryption required by keyboard: %s", gerr->message);
+                g_error_free(gerr);
+                return -EFAULT;
+            }*/
+
             break;
     }
     return 0;
+
+}
+
+static void shutdown_channels(struct input_host *host){
+    if (host->ctrl_watch > 0)
+        g_source_remove(host->ctrl_watch);
+    host->ctrl_watch = 0;
+
+    if (host->intr_watch > 0)
+        g_source_remove(host->intr_watch);
+    host->intr_watch = 0;
+
+    if (host->intr_io) {
+        g_io_channel_shutdown(host->intr_io, TRUE, NULL);
+        g_io_channel_unref(host->intr_io);
+        host->intr_io = NULL;
+    }
+
+    if (host->ctrl_io) {
+        g_io_channel_shutdown(host->ctrl_io, TRUE, NULL);
+        g_io_channel_unref(host->ctrl_io);
+        host->ctrl_io = NULL;
+    }
+
 
 }
 
@@ -140,18 +199,9 @@ int input_host_remove(const bdaddr_t *src, const bdaddr_t *dst)
     ba2str(dst, address);
     DBG("Removing input host %s", address);
 
-    if (host->intr_io) {
-        g_io_channel_shutdown(host->intr_io, TRUE, NULL);
-        g_io_channel_unref(host->intr_io);
-        host->intr_io = NULL;
-    }
+    shutdown_channels(host);
 
-    if (host->ctrl_io) {
-        g_io_channel_shutdown(host->ctrl_io, TRUE, NULL);
-        g_io_channel_unref(host->ctrl_io);
-        host->ctrl_io = NULL;
-    }
-    btd_adapter_remove_device(adapter_find(&host->src), host->device);
+    //btd_adapter_remove_device(adapter_find(&host->src), host->device);
     btd_device_unref(host->device);
     g_free(host->path);
 
